8be52289210cc80964d03694beba48633409b680
Ruslan <timbaraccoon@gmail.com>
2020-09-02 23:16:57 +0500

    ...

---
+++ src/BruteCollinearPoints.java
@@ -0,0 +1,18 @@
+public class BruteCollinearPoints {
+
+    public BruteCollinearPoints(Point[] points) {
+
+    }    // finds all line segments containing 4 points
+
+
+    public int numberOfSegments() {
+
+        return 0;
+    }        // the number of line segments
+
+
+    public LineSegment[] segments() {
+
+        return null;
+    }                // the line segments
+}
---
+++ src/FastCollinearPoints.java
@@ -0,0 +1,19 @@
+public class FastCollinearPoints {
+
+    public FastCollinearPoints(Point[] points) {
+
+
+    }    // finds all line segments containing 4 or more points
+
+
+    public int numberOfSegments() {
+
+        return 0;
+    }       // the number of line segments
+
+
+    public LineSegment[] segments() {
+
+        return null;
+    }               // the line segments
+}
